cddea7dfdf5428f48721e6d1167e98ccf675352c
Amitav Nott <amitav_nott@ryecountryday.org>
2020-12-10 07:53:43 -0500

    Removing unnecessary functions and switching more logic to lua.

--- lua/termight/init.lua
+++ lua/termight/init.lua
@@ -5,7 +5,8 @@ local function initializeNewTerm()
     local idx = vim.g["idx"]
     vim.cmd("let g:term_buf_list[g:idx - 1] = "  .. vim.api.nvim_create_buf(true, false))
     bufs = vim.g["term_buf_list"]
-    vim.api.nvim_exec(bufs[idx] .. "buf! | term", nil)
+    vim.api.nvim_set_current_buf(bufs[idx])
+    vim.api.nvim_exec(":term", {})
 end
 
 local function bufExists(idx)
@@ -31,22 +32,6 @@ local function openTerm()
   end
 end
 
-local function createTerm()
-  local bufh = vim.api.nvim_create_buf(true, false)
-  table.insert(bufs, bufh);
-  vim.api.nvim_exec("sp | " .. bufh .. "buf! | term", nil)
-end
-
-local function closeTerm()
-  -- local cbuf = vim.api.nvim_get_current_buf()
-
-  if string.find(vim.api.nvim_buf_get_name({}), "term:/", 1, true) then
-    vim.api.nvim_exec(":bp", nil)
-  end
-end
-
 return {
-  createTermBuffer = createTerm,
   openTerm = openTerm,
-  closeTerm = closeTerm,
 }
--- plugin/termight.vim
+++ plugin/termight.vim
@@ -5,20 +5,10 @@ nnoremap <leader>2 :call OpenTerm(2)<CR>
 nnoremap <leader>3 :call OpenTerm(3)<CR>
 nnoremap <leader>4 :call OpenTerm(4)<CR>
 
-fun! CreateTerm()
-  lua for k in pairs(package.loaded) do if k:match("^buf%-manager") then package.loaded[k] = nil end end
-  lua require("termight").createTermBuffer();
-endfun
-
 fun! OpenTerm(idx)
   let g:idx = a:idx
   lua for k in pairs(package.loaded) do if k:match("^buf%-manager") then package.loaded[k] = nil end end
   lua require("termight").openTerm();
-endfun
-
-fun! CloseTerm()
-  lua for k in pairs(package.loaded) do if k:match("^buf%-manager") then package.loaded[k] = nil end end
-  lua require("termight").closeTerm();
 endfun
 
 augroup YourFirstPlugin
