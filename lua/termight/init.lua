b97c43fd5168677c533a613407b5dfa430863329
Amitav Nott <amitav_nott@ryecountryday.org>
2021-01-03 19:19:11 -0500

    added the RunShellCommand editor command

--- lua/termight/init.lua
+++ lua/termight/init.lua
@@ -11,11 +11,9 @@ local function initializeNewTerm()
 end
 
 local function bufExists(idx)
-    for i=1, #vim.api.nvim_list_bufs() do
-      if bufs[idx] == vim.api.nvim_list_bufs()[i] then
+  for i=1, #vim.api.nvim_list_bufs() do if bufs[idx] == vim.api.nvim_list_bufs()[i] then
         return true
-      end
-    end
+      end end
     return false
 end
 
@@ -24,14 +22,63 @@ local function openTerm()
   if idx and idx <= #bufs then
     if bufs[idx] == 0 or not bufExists(idx) then
       initializeNewTerm()
-    else
-      vim.api.nvim_exec(bufs[idx] .. "buf!", nil)
-    end
+    else vim.api.nvim_exec(bufs[idx] .. "buf!", nil) end
   else
     print("invalid index")
   end
 end
 
+-- run a shell command and view the output in a floating window
+local function runShellCommand()
+  local cmd = vim.g["termight_cmd"]
+
+  -- getting the current window width and height
+  -- as well as defining the factor size the floating
+  -- window on
+  local stats = vim.api.nvim_list_uis()[1]
+  local width = stats.width;
+  local height = stats.height;
+  local factor = 0.9;
+
+  -- creating a new buffer
+  local bufh = vim.api.nvim_create_buf(false, true)
+  -- creating the window
+  vim.api.nvim_open_win(bufh, true, {
+    relative = "editor",
+    width    = math.floor(width * factor),
+    height   = math.floor(height * factor),
+    col      = math.floor(width * (1 - factor) / 2),
+    row      = math.floor(height * (1 - factor) / 2)
+  })
+  vim.api.nvim_exec("set nonu norelativenumber signcolumn=no colorcolumn=", nil)
+
+  vim.cmd(":term")
+  -- definitions of some key sequences
+  local carriage_return = vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+  local exit_term = vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true)
+
+  -- a super hacky way to remove the prompt from the window
+  vim.api.nvim_feedkeys("aexport PS1='' && clear", "n", {})
+  vim.api.nvim_feedkeys(carriage_return, "n", {})
+  vim.api.nvim_feedkeys(exit_term, "n", {})
+
+  -- insert the command into the terminal
+  vim.api.nvim_feedkeys("a" .. cmd, "n", {})
+  vim.api.nvim_feedkeys(carriage_return, "n", {})
+
+  -- exit insert mode
+  vim.api.nvim_feedkeys(exit_term, "n", {})
+
+  -- bindings: esc to close the window, disable ways to enter insert mode
+  vim.api.nvim_buf_set_keymap(bufh, "n", "<ESC>",
+    "<CMD>lua vim.api.nvim_buf_delete("..bufh..", {force=true})<CR>", {})
+  vim.api.nvim_buf_set_keymap(bufh, "n", "i", "<nop>", {})
+  vim.api.nvim_buf_set_keymap(bufh, "n", "I", "<nop>", {})
+  vim.api.nvim_buf_set_keymap(bufh, "n", "a", "<nop>", {})
+  vim.api.nvim_buf_set_keymap(bufh, "n", "A", "<nop>", {})
+end
+
 return {
   openTerm = openTerm,
+  runShellCommand = runShellCommand
 }
--- plugin/termight.vim
+++ plugin/termight.vim
@@ -1,10 +1,17 @@
 let g:term_buf_list = [0, 0, 0, 0]
 
 :command -nargs=1 OpenTerm :call OpenTerm(<args>)
+:command -nargs=1 RunShellCommand :call RunShellCommand(<args>)
 
 fun! OpenTerm(idx)
   let g:idx = a:idx
   lua require("termight").openTerm();
+endfun
+
+fun! RunShellCommand(cmd)
+  lua for k in pairs(package.loaded) do if k:match("^termight") then package.loaded[k] = nil end end
+  let g:termight_cmd = a:cmd
+  lua require("termight").runShellCommand()
 endfun
 
 augroup YourFirstPlugin
